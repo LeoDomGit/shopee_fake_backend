31e15e25a18eac9fb42c0fa7623626eda49a4069
Leo Git <leodomsolar@gmail.com>
2024-12-04 13:50:12 +0700

    Update 2024_12_04_033656_update_products_table.php

--- database/migrations/2024_12_04_033656_update_products_table.php
+++ database/migrations/2024_12_04_033656_update_products_table.php
@@ -19,7 +19,6 @@ return new class extends Migration
                 $table->foreign('brand_id')->references('id')->on('brands');
                 $table->foreign('category_id')->references('id')->on('categories');
                 $table->foreign('shop_id')->references('id')->on('shops');
-
             });
         }
     }
